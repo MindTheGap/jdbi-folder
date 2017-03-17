0ee8be76269926161aae78cc58e8ff9e80fcb345
Chen Avnery <cavnery@gmail.com>
2017-03-17 19:56:05 +0200

    added fieldName on exception

--- src/main/java/com/github/rkmk/mapper/FieldHelper.java
+++ src/main/java/com/github/rkmk/mapper/FieldHelper.java
@@ -72,16 +72,16 @@ public class FieldHelper {
         try {
             field = o.getClass().getDeclaredField(fieldName);
         } catch (NoSuchFieldException e) {
-            throw new IllegalArgumentException("The field must not be null");
+            throw new IllegalArgumentException("The field (" + fieldName + ") must not be null");
         }
         if (field == null) {
-            throw new IllegalArgumentException("The field must not be null");
+            throw new IllegalArgumentException("The field (" + fieldName + ") must not be null");
         }
         field.setAccessible(true);
         try {
             return field.get(o);
         } catch (IllegalAccessException e) {
-            throw new IllegalArgumentException("The field not accessible");
+            throw new IllegalArgumentException("The field (" + fieldName + ") not accessible");
         }
     }
 
